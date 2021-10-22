b2b9e6e379d6ec809f7d4420290dc13dee7ef95a
Ron Lawhorn <r.e.lawhorn@gmail.com>
2021-10-22 10:54:28 -0400

    Early working model still needs groups added to users and KBA added to home pages

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    val_password = PasswordField(
+        "Repeat Password", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('That username already exists. Please try a different user name')
+
+    def validate_email(self, email):
+        email_address = User.query.filter_by(email=email.data).first()
+        if email_address is not None:
+            raise ValidationError('That email address is in use, please select another')
--- app/models.py
+++ app/models.py
@@ -1,11 +1,50 @@
-from app import db
+from app import db, login
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    groups = db.relationship('Group', backref='members', lazy='dynamic')
+    kbas = db.relationship('KBA', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f'''<{self.username} >'''
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group = db.Column(db.String(32), index=True, unique=True)
+    geckos = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'''<Group Name: {self.group}
+        Geckos: {self.geckos}>'''
+
+
+class KBA(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(126), index=True, unique=True)
+    body = db.Column(db.String(1000), index=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'''<Title {self.title} - Date {self.timestamp} - Author {self.author}
+ {self.body}>'''
